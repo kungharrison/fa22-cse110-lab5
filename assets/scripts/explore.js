d714281158881e023c9a7e371fb0ffd8bdcc929b
Harrison K <53415491+kungharrison@users.noreply.github.com>
2022-10-27 00:37:30 -0700

    completed speech synthesis module

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,52 @@
 // explore.js
-
-window.addEventListener('DOMContentLoaded', init);
+    window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    // wait for voices to load
+    window.speechSynthesis.onvoiceschanged = function() {
+        let voices = speechSynthesis.getVoices();
+        for (let i = 0; i < voices.length; i++) {
+            let option = document.createElement('option');
+            option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+            document.getElementById('voice-select').appendChild(option);
+        }
+    }
+    
+    // run text to speech when press to talk is pressed
+    document.addEventListener('click', (event) => {
+        if (event.target === document.querySelector('button')) {
+            // get text from textarea
+            let text = document.getElementById('text-to-speak').value;
+            let utterance = new SpeechSynthesisUtterance(text);
+            // change voice type
+            let voice = document.getElementById('voice-select').value;
+            let voices = speechSynthesis.getVoices();
+            for (let i = 0; i < voices.length; i++) {
+                if (voice === voices[i].name + ' (' + voices[i].lang + ')') {
+                    speechSynthesis.cancel();
+                    utterance = new SpeechSynthesisUtterance(document.getElementById('text-to-speak').value);
+                    utterance.voice = voices[i];
+                }
+            }
+            speechSynthesis.speak(utterance);
+        }
+    false});
+
+    // change face image to talking face
+    document.addEventListener('click', (event) => {
+        if (event.target === document.querySelector('button')) {
+            let image = document.querySelector('img');
+            image.src = './assets/images/smiling-open.png';
+        }
+    false});
+
+    // change face to normal face
+    document.addEventListener('click', (event) => {
+        if (event.target === document.querySelector('button')) {
+            let image = document.querySelector('img');
+            setTimeout(function() {
+                image.src = './assets/images/smiling.png';
+            }, 1000);
+        }
+    false});
 }
